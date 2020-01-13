8688c1344be4c14ff72783c2ad54c927bde0eab3
Ficko <filip-f@hotmail.com>
2020-01-13 18:48:43 +0100

    logged in feature

--- src/ApiFacade.js
+++ src/ApiFacade.js
@@ -17,6 +17,14 @@ class ApiFacade {
         return fetch(URL + "m7/cycling", options).then(handleHttpErrors);
     }
 
+    login = (user, pass) => {
+        const options = this.makeOptions("POST", true, { userName: user, userPassword: pass });
+        return fetch(URL + "login/", options, true)
+          .then(handleHttpErrors)
+          .then(res => { this.setToken(res.token); console.log(res.token); })
+          
+      }
+
     makeOptions(method, addToken, body) {
         var opts = {
             method: method,
@@ -25,11 +33,32 @@ class ApiFacade {
                 'Accept': 'application/json',
             }
         }
+        if (addToken && this.loggedIn()) {
+            opts.headers["x-access-token"] = this.getToken();
+          }
+
         if (body) {
             opts.body = JSON.stringify(body);
         }
         return opts;
     }
+
+    setToken = (token) => {
+        localStorage.setItem('jwtToken', token)
+      }
+      getToken = () => {
+        return localStorage.getItem('jwtToken')
+      }
+
+      loggedIn = () => {
+        const loggedIn = this.getToken() != null;
+        return loggedIn;
+      }
+      logout = () => {
+        localStorage.removeItem("jwtToken");
+      }
+
+
 }
 const facade = new ApiFacade();
 export default facade;
--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
 import { ShowError, ShowNeutral, ShowSuccess } from './ErrorMessage';
+import facade from "./ApiFacade.js"
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loggedIn: false }
+  }
+  logout = () => {
+    facade.logout();
+    this.setState({ loggedIn: false });
+  }
+  login = (user, pass) => {
+    facade.login(user, pass)
+      .then(res => this.setState({ loggedIn: true }));
+  }
+  render() {
+    return (
+      <div className="container">
+        {!this.state.loggedIn ? (<LogIn login={this.login} />) :
+          (<div>
+            <LoggedIn />
+            <button onClick={this.logout}>Logout</button>
+          </div>)}
+      </div>
+    )
+  }
+}
+
+
+class LogIn extends Component {
     constructor(props) {
       super(props);
       this.state = { username: "", password: "", clicked: false }
     }
     login = (evt) => {
+      
       evt.preventDefault();
       this.props.login(this.state.username, this.state.password);
+      console.log(this.state.username)
+      console.log(this.state.password)
     }
     onChange = (evt) => {
       this.setState({ [evt.target.id]: evt.target.value })
@@ -42,5 +73,25 @@ class Login extends Component {
     )
   }
 }
+
+
+class LoggedIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { dataFromServer: "Fetching!!" };
+  }
+  componentDidMount() {
+    //facade.fetchData().then(res => this.setState({ dataFromServer: res }));
+  }
+  render() {
+    return (
+      <div>
+        <h1>logged in homie</h1>
+      </div>
+    )
+  }
+}
+
+
 
 export default Login
